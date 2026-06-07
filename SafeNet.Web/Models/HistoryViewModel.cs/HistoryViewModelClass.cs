3c143a003a32ca2bd2e4d7cd80bc67bcd0696e84
Tefa60 <dannaeemt@gmail.com>
2026-06-07 17:10:09 -0500

    Sprint 2: Detail.cshtml + HistoryViewModel + History views

---
+++ SafeNet.Web/Models/HistoryViewModel.cs/HistoryViewModelClass.cs
@@ -0,0 +1,14 @@
+﻿using SafeNet.Data.Entidades;
+
+namespace SafeNet.Web.Models.ViewModels
+{
+    public class HistoryViewModel
+    {
+        public List<AnalysisEntity> Analyses { get; set; } = new();
+        public AnalysisEntity? SelectedAnalysis { get; set; }
+        public string? FilterVerdict { get; set; }
+        public string? FilterType { get; set; }
+        public DateTime? FilterFrom { get; set; }
+        public DateTime? FilterTo { get; set; }
+    }
+}
